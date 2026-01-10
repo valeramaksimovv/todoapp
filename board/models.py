17ca86447571847e69da464fd787b206030829c5
ValeraMaksimoVV <valeramaksimovv@gmail.com>
2026-01-10 21:17:48 +0100

    add board & create model card in board/models.py

---
+++ board/models.py
@@ -0,0 +1,74 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+
+class Card(models.Model):
+    class Status(models.TextChoices):
+        BACKLOG = "BACKLOG", "Backlog"
+        TO DO = "TODO", "To Do"
+        IN_PROGRESS = "IN_PROGRESS", "In Progress"
+        REVIEW = "REVIEW", "Reviev"
+        DONE = "DONE", "Done"
+
+    class Priority(models.TextChoices):
+        HIGH = "HIGH", "High"
+        MEDIUM = "MEDIUM", "Medium"
+        LOW = "LOW", "Low"
+
+# name card
+    name = models.CharField(max_length=250)
+
+# status card, BACKLOG default status
+    Status = models.CharField(
+        max_length=20,
+        choices=Status.choises,
+        default=Status.BACKLOG
+    )
+
+# priority card, MEDIUM default priority
+    Priority = models.CharField(
+        max_lenght=10,
+        choices=Priority.choices
+        default=Priority.MEDIUM
+    )
+
+# assign card
+    assigne = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="assigned_cards",
+    )
+
+# reported card
+    reporter = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reported_cards",
+    )
+
+# description card, otional field
+    description = models.TextField(blank=True)
+
+# create and update time
+    create_at = models.DataTimeField(auto_now_add=True)
+    update_at = models.DataTimeField(auto_now=True)
+
+# create by
+    create_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="created_cards",
+    )
+
+# last update by
+    last_update_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="updated_cards",
+    )
+
+def __str__(self) -> str:
+    return self.name
